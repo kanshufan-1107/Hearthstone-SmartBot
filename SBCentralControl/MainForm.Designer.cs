6a9e4b9b10e280db4884af1dcc12c51893854863
范广军 <1536576901@qq.com>
2022-03-31 00:20:40 +0800

    开发中控窗口

---
+++ SBCentralControl/MainForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SBCentralControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 207);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "中控核心设置";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 20);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(312, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.Text1_change);
+            // 
+            // label3
+            // 
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Location = new System.Drawing.Point(6, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "炉石窗口名称";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Location = new System.Drawing.Point(108, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(295, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "分钟，定时检测炉石和SB是否已异常，如有则重启应用";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.Text2_change);
+            // 
+            // label1
+            // 
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "检测频率";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "测试";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 512);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "SB中控";
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SBCentralControl/MainForm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SBCentralControl
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// 中控检测炉石和SB的频率，默认为5分钟
+        /// </summary>
+        int DetectionFrequency = 5;
+        /// <summary>
+        /// 炉石传说窗口名称
+        /// </summary>
+        string lscs_ckmc = "炉石传说";
+        /// <summary>
+        /// 防检测程序文件夹
+        /// </summary>
+        private static readonly string BasePath = Environment.CurrentDirectory + Path.DirectorySeparatorChar.ToString() + "Temp" + Path.DirectorySeparatorChar.ToString() + "name.temp";
+        /// <summary>
+        /// 启动器所在地址
+        /// </summary>
+        private static readonly string LauncherPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar.ToString() + "Launcher.exe";
+
+        /// <summary>
+        /// 获取窗口句柄
+        /// </summary>
+        /// <param name="lpClassName"></param>
+        /// <param name="lpWindowName"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        /// <summary>
+        /// 确定系统是否认为指定的应用程序没有响应
+        /// </summary>
+        /// <param name="hWnd">要测试的窗口的句柄</param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern bool IsHungAppWindow(IntPtr hWnd);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //初始化页面信息
+            textBox1.Text = DetectionFrequency.ToString();
+            textBox2.Text = lscs_ckmc;
+            bool flag = IsNumberic(textBox1.Text);
+            if (flag)
+            {
+                timer1.Interval = 1000 * 60 * int.Parse(textBox1.Text);
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void MainForm_Activated(object sender, EventArgs e)
+        {
+            //防止输入框初始焦点
+            groupBox1.Focus();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            /*string text = string.Empty;
+            if (File.Exists(BasePath))
+            {
+                text = File.ReadAllText(BasePath);
+            }
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                Process[] processesByName = Process.GetProcessesByName(text.Replace(".exe", ""));
+                foreach (Process process in processesByName)
+                {
+                    bool processHungFlag = IsHungAppWindow(process.Handle);
+                    if (processHungFlag)
+                    {
+                        //关闭SB
+                        process.Kill();
+                        //打开SB
+                        Process newProcess = new Process
+                        {
+                            StartInfo = new ProcessStartInfo(LauncherPath)
+                        };
+                        newProcess.Start();
+                    }
+                }
+            }*/
+            /*//获取炉石传说句柄
+            IntPtr lscs = FindWindow(null, lscs_ckmc);
+            bool hungFlag = IsHungAppWindow(lscs);
+            if (hungFlag)
+            {
+                //关闭炉石传说
+                System.Threading.Thread.Sleep(5000);
+                //开启炉石传说
+            }*/
+        }
+
+        private void Text1_change(object sender, EventArgs e)
+        {
+            lscs_ckmc = textBox2.Text;
+        }
+
+        private void Text2_change(object sender, EventArgs e)
+        {
+            bool flag = IsNumberic(textBox1.Text);
+            if (flag)
+            {
+                DetectionFrequency = int.Parse(textBox1.Text);
+                timer1.Interval = 1000 * 60 * int.Parse(textBox1.Text);
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string text = string.Empty;
+            if (File.Exists(BasePath))
+            {
+                text = File.ReadAllText(BasePath);
+            }
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                Process[] processesByName = Process.GetProcessesByName(text.Replace(".exe", ""));
+                foreach (Process process in processesByName)
+                {
+                    //关闭SB
+                    process.Kill();
+                    System.Threading.Thread.Sleep(2000);
+                    //打开SB
+                    Process newProcess = new Process
+                    {
+                        StartInfo = new ProcessStartInfo(LauncherPath)
+                    };
+                    newProcess.Start();
+                }
+            }
+        }
+
+        protected bool IsNumberic(string message)
+        {
+            System.Text.RegularExpressions.Regex rex =
+            new System.Text.RegularExpressions.Regex(@"^\d+$");
+            if (rex.IsMatch(message))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
